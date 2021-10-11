c7e8d06c72309708069df185adcb6db3bef12a8d
Michel DUPOUY <michel.dupouy2021@campus-eni.fr>
2021-10-11 16:43:00 +0200

    encore oui

---
+++ src/Entity/Site.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SiteRepository::class)
+ */
+class Site
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Sortie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getSortie(): ?string
+    {
+        return $this->Sortie;
+    }
+
+    public function setSortie(string $Sortie): self
+    {
+        $this->Sortie = $Sortie;
+
+        return $this;
+    }
+}
