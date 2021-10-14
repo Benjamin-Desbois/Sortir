e1cd8a3248515ecb611266f080a31abc1ed4382e
Michel DUPOUY <michel.dupouy2021@campus-eni.fr>
2021-10-14 15:45:18 +0200

    Modification profil

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -27,7 +27,7 @@ class RegistrationController extends AbstractController
             $user->setActif(false);
             $user->setAdministrateur(false);
             $user->setPassword(
-            $userPasswordHasherInterface->hashPassword(
+                $userPasswordHasherInterface->hashPassword(
                     $user,
                     $form->get('plainPassword')->getData()
                 )
@@ -47,13 +47,66 @@ class RegistrationController extends AbstractController
     }
 
     /** @Route ("/profil/{id}", name="profilDetail", requirements={"id":"\d+"}) */
-    public function detail($id, ParticipantRepository $repository, SiteRepository  $siteRepo): Response{
+    public function detail($id, ParticipantRepository $repository): Response
+    {
         $user = $repository->find($id);
         $site = $user->getSitesNoSite();
 
-        $user->setSitesNoSite($siteRepo->find($user->getSitesNoSite()->getId()));
+        return $this->render('main/profil.html.twig', ['user' => $user, 'site' => $site]);
+    }
 
-        //dd($user);
-        return $this->render('main/profil.html.twig', ['user'=>$user, 'site'=>$site]);
+    /** @Route ("/profil", name="app_profil") */
+    public function monProfil(): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $site = $user->getSitesNoSite();
+
+        return $this->render('main/profil.html.twig', ['user' => $user, 'site' => $site]);
+    }
+
+    /** @Route ("/modifierProfil", name="app_modifierProfil") */
+    public function modifierProfil(SiteRepository $repo): Response
+    {
+        {
+                $user = $this->getUser();
+                $site = $user->getSitesNoSite();
+                $all = $repo->findAll();
+
+            if ( isset( $_POST['modifier'] )) {
+
+                if ( $_POST['pseudo'] != null) {
+                    $user->setPseudo($_POST['pseudo']);
+                }
+                if ( $_POST['nom'] != null) {
+                    $user->setNom($_POST['nom']);
+                }
+                if ( $_POST['prenom'] != null) {
+                    $user->setPrenom($_POST['prenom']);
+                }
+                if ( $_POST['numTel'] != null) {
+                    $user->setTelephone($_POST['numTel']);
+                }
+                if ( $_POST['email'] != null) {
+                    $user->setMail($_POST['email']);
+                }
+                if ( $_POST['site'] != null) {
+                    $noSite = $repo->findOneBy(array('nom'=>$_POST['site']));
+                    $user->setSitesNoSite($noSite);
+                }
+                $user = $this->getUser();
+                $site = $user->getSitesNoSite();
+                $all = $repo->findAll();
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }
+
+
+            return $this->render('main/modifierProfil.html.twig', ['user' => $user, 'site' => $site, 'all' => $all]);
+        }
     }
 }
